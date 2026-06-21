6d23a418d3c7a4cafb5d745440ad29af145de4dd
Shmurda9 <orchardglenbernie@gmail.com>
2026-06-21 04:43:45 -0700

    fix: resolve permissive trust proxy validation crash

--- src/server.ts
+++ src/server.ts
@@ -41,6 +41,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
+  validate: { trustProxy: false }, // 👈 ADD THIS LINE HERE TO FIX THE CRASH
 });
 app.use("/api/", limiter);
 
